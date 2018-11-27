4add07ed86263b317a9c7a8d58710814629a549d
ztl <zhangtianlong019@163.com>
2018-11-27 16:03:04 +0800

    u

--- 1.py
+++ 1.py
@@ -13,29 +13,4 @@ def aa():
     
     
     b='''dsfdsafdasfadsfasdf
-'''
-
-    
-    
-    
-    
-    
-    
-    print('dasfdsafdasfadsfadsgasrha32erhQ34Y2Y253YYW5Y3')
-
-    fdjasfkasdfkhdaskfhkjdasfkdgaskfasf
-    
-    
-    kdsafhdkahksadfhkasd134333333333
-    
-    
-    sadfadsfadsfdasfadsfads
-    
-    
-    
-    dafdasfdasfdsafsdafs
-    
-    
-    
-    111111111111111111111111111111111111111111111111111
 ''
